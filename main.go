2892247e85e320b6556bd77d2f182990c8d0a2cb
dymyw <dymayongwei@163.com>
2020-08-16 11:06:28 +0800

    router

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	// 先执行 boot 里面的 init 方法
 	_ "github.com/gogf/gf-demos/boot"
 
+	// 执行 router 里面的 init 方法
 	_ "github.com/gogf/gf-demos/router"
 	"github.com/gogf/gf/frame/g"
 )
--- router/router.go
+++ router/router.go
@@ -11,21 +11,30 @@ import (
 
 // 你可以将路由注册放到一个文件中管理，
 // 也可以按照模块拆分到不同的文件中管理，
-// 但统一都放到router目录下。
+// 但统一都放到 router 目录下。
 func init() {
+	// 返回一个 ghttp.Server 实例
 	s := g.Server()
 
-	// 某些浏览器直接请求favicon.ico文件，特别是产生404时
+	// 某些浏览器直接请求 favicon.ico 文件，特别是产生 404 时
 	s.SetRewrite("/favicon.ico", "/resource/image/favicon.ico")
+	// SetRewrite 可以为一些静态资源重写路由
 
-	// 分组路由注册方式
+	// 分组路由注册方式，可以给分组路由指定一个 prefix 前缀
+	// https://goframe.org/net/ghttp/group/index
 	s.Group("/", func(group *ghttp.RouterGroup) {
+		// func new(Type) *Type 返回一个执行该类型的指针
 		ctlChat := new(chat.Controller)
 		ctlUser := new(user.Controller)
+
+		// 给路由分组绑定一个或多个中间件
 		group.Middleware(middleware.CORS)
+
+		// ALL 代表允许所有的请求方式，如 GET、POST 等都可以
 		group.ALL("/chat", ctlChat)
 		group.ALL("/user", ctlUser)
 		group.ALL("/curd/:table", new(curd.Controller))
+
 		group.Group("/", func(group *ghttp.RouterGroup) {
 			group.Middleware(middleware.Auth)
 			group.ALL("/user/profile", ctlUser, "Profile")
