3d1680748d229393e813ac3e23a893065e098beb
dymyw <dymayongwei@163.com>
2020-08-16 08:29:45 +0800

    packed

--- boot/boot.go
+++ boot/boot.go
@@ -1,6 +1,8 @@
 package boot
 
 import (
+	// 先执行 packed 里面的 init 方法
+	// 【【【 这里建议引入 packed 包和其他包之间加入一个空行以作区分，特别是 Goland IDE 的 import 插件不会将引入包进行自动排序 】】】
 	_ "github.com/gogf/gf-demos/packed"
 
 	"github.com/gogf/gf/frame/g"
--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	// 先执行 boot 里面的 init 方法
 	_ "github.com/gogf/gf-demos/boot"
+
 	_ "github.com/gogf/gf-demos/router"
 	"github.com/gogf/gf/frame/g"
 )
